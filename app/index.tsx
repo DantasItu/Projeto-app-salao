cf33002ea43fd10f002eb64f7092a2800e268218
DantasItu <michael.of.douglasaraujodantas5@gmail.com>
2026-04-16 14:16:24 -0300

    criação do botão e função de logount

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const Router = useRouter();
+
+  //   Função pra sair do login
+  const handleLogount = async () => {
+    await SecureStore.deleteItemAsync("userToken"); // isso limpa o token dos arquivos
+    Router.replace("/login");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Salão Pro</Text>
       <Text>Gerenciamento de Agenda e Estoque</Text>
+      <Button title="sair" onPress={handleLogount} />
     </View>
   );
 }
