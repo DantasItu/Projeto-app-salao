0ff1fe34c06ce1278bc5901d98324be2fa6911aa
DantasItu <michael.of.douglasaraujodantas5@gmail.com>
2026-04-16 19:40:13 -0300

    remoção da barra padrão de cabeçalho  do router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Início" }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
     </Stack>
   );
